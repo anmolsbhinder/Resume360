1ac51adbd432dc75c1e44ed599e4daa1d9d7c6b3
Heikrana <49754221+Heikrana@users.noreply.github.com>
2023-08-19 17:18:54 +0530

    Fix formatting
    The pre-commits hooks were not working. Manually fixed formatting.

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,29 +1,28 @@
 // src/api.ts
-import axios from 'axios';
+import axios from "axios";
 
-const githubToken = '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'; // Replace with your GitHub token
+const githubToken = "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"; // Replace with your GitHub token
 
 const fetchGitHubUserInfo = async (username: string) => {
-  try {
-    const [userInfoResponse, starsResponse] = await Promise.all([
-        axios.get(`https://api.github.com/users/${username}`, {
-          headers: {
-            Authorization: `token ${githubToken}`,
-          },
-        }),
-        axios.get(`https://api.github.com/users/${username}/starred`, {
-          headers: {
-            Authorization: `token ${githubToken}`,
-          },
-        }),
-      ]);
-      
-    //   console.log(userInfoResponse.data);
-      return { userInfo: userInfoResponse.data, stars: starsResponse.data };
-  } catch (error) {
-    throw new Error('Error fetching GitHub user info');
-  }
-};
+	try {
+		const [userInfoResponse, starsResponse] = await Promise.all([
+			axios.get(`https://api.github.com/users/${username}`, {
+				headers: {
+					Authorization: `token ${githubToken}`,
+				},
+			}),
+			axios.get(`https://api.github.com/users/${username}/starred`, {
+				headers: {
+					Authorization: `token ${githubToken}`,
+				},
+			}),
+		]);
 
+		//   console.log(userInfoResponse.data);
+		return { userInfo: userInfoResponse.data, stars: starsResponse.data };
+	} catch (error) {
+		throw new Error("Error fetching GitHub user info");
+	}
+};
 
 export { fetchGitHubUserInfo };
